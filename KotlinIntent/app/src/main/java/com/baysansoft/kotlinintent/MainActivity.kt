9e30821c7e42130ef8ea69a3e014182bafc71c41
mebaysan <menesbaysan@gmail.com>
2020-07-10 18:54:04 +0300

    activity lifecycle

--- KotlinIntent/app/src/main/java/com/baysansoft/kotlinintent/MainActivity.kt
+++ KotlinIntent/app/src/main/java/com/baysansoft/kotlinintent/MainActivity.kt
@@ -4,11 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { // bir aktivite *ilk* açılırken bu metot çalışır
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("on create called")
+
+    }
+
+    override fun onResume() { // bir aktivite çalışırken bu metot çalışır
+        super.onResume()
+        println("on resume called")
+    }
+
+    override fun onPause() { // bir aktiviteden başka bir aktiviteye geçiş esnasında bu metot çalışır
+        super.onPause()
+        println("on pause called")
+
+    }
+
+    override fun onStop() { // bir aktiviteden diğer bir aktiviteye geçildiğinde bu metot çağrılır
+        super.onStop()
+        println("on stop called")
+
+    }
+
+    override fun onDestroy() { // bir aktivite tamamen kapatıldığında bu metot çağrılır
+        super.onDestroy()
+        println("on destroy called")
+
     }
 
     fun funLogin(view: View) {
@@ -16,6 +42,12 @@ class MainActivity : AppCompatActivity() {
             applicationContext,
             MyCustomActivity::class.java
         ) // activity'ler arasında geçiş yapabilmemizi sağlar
+        intent.putExtra(
+            "username",
+            editTextUsername.text.toString()
+        ) // key-value şeklinde bu değerleri Intent içerisine ekliyoruz
+        intent.putExtra("name", editTextName.text.toString())
         startActivity(intent) // belirttiğimiz sınıfa bizi götürecektir
+        finish() // bu aktiviteyi bitirir/kapatır 
     }
 }
--- KotlinIntent/app/src/main/java/com/baysansoft/kotlinintent/MyCustomActivity.kt
+++ KotlinIntent/app/src/main/java/com/baysansoft/kotlinintent/MyCustomActivity.kt
@@ -2,10 +2,20 @@ package com.baysansoft.kotlinintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_my_custom.*
 
 class MyCustomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_custom)
+
+        //get intent
+        val intent = intent
+        val username =
+            intent.getStringExtra("username") // key-value şeklinde eklediğimiz data'ları intent içerisinden alıyoruz
+        val name = intent.getStringExtra("name")
+
+        textName.text = name
+        textUsername.text = username
     }
 }
