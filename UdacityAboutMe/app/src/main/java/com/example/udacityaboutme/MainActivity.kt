8db9dd982974479d77b5b8fd537cc997d0c47632
mebaysan <menesbaysan@gmail.com>
2020-07-19 18:06:29 +0300

    yarida kaldi

--- UdacityAboutMe/app/src/main/java/com/example/udacityaboutme/MainActivity.kt
+++ UdacityAboutMe/app/src/main/java/com/example/udacityaboutme/MainActivity.kt
@@ -8,28 +8,49 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.udacityaboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Baysan")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener { view -> // bu şekilde parametreyi kendimiz isimlendirebiliriz
-            // button click dinleyici
-            addNickname(view)
+//        findViewById<Button>(R.id.done_button).setOnClickListener { view -> // bu şekilde parametreyi kendimiz isimlendirebiliriz
+//            // button click dinleyici
+//            addNickname(view)
+//        }
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
         }
 
     }
 
     private fun addNickname(view: View) { // b.r view bu fonksiyonu kullanacağı için parametre olarak view alıyoruz
-        val editText =
-            findViewById<EditText>(R.id.nickname_edit) // nickname_edit id'li view'ı değişkene atadık
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+//        val editText =
+//            findViewById<EditText>(R.id.nickname_edit) // nickname_edit id'li view'ı değişkene atadık
+//        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+//
+//        nicknameTextView.text = editText.text // text'i değiştirdik
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE // bu fonksiyonu kullanan view'ın görünümünü gizledik
+//        nicknameTextView.visibility =
+//            View.VISIBLE // görünümü gizli olan nicknameTextView'ın görünümünü açığa çıkardık
 
-        nicknameTextView.text = editText.text // text'i değiştirdik
-        editText.visibility = View.GONE
-        view.visibility = View.GONE // bu fonksiyonu kullanan view'ın görünümünü gizledik
-        nicknameTextView.visibility =
-            View.VISIBLE // görünümü gizli olan nicknameTextView'ın görünümünü açığa çıkardık
+        binding.apply {
+//            binding.nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
 
         // klavyeyi gizlemek
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
---
+++ UdacityAboutMe/app/src/main/java/com/example/udacityaboutme/MyName.kt
@@ -0,0 +1,3 @@
+package com.example.udacityaboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
